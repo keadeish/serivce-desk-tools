56b56120913f5c1825f0e3cef74766a2b3d8e518
keadeish <90222144+keadeish@users.noreply.github.com>
2024-04-10 13:22:36 +0100

    Adjusted formatting & default XAML location

--- ServiceDeskTools.ps1
+++ ServiceDeskTools.ps1
@@ -1,6 +1,6 @@
 Add-Type -AssemblyName PresentationFramework
 
-$xamlFile="C:\Users\kmorrison\Development\Service Desk Tools\MainWindow.xaml"
+$xamlFile="C:\Temp\MainWindow.xaml"
 $inputXAML=Get-Content -Path $xamlFile -Raw
 $inputXAML=$inputXAML -replace 'mc:Ignorable="d"', '' -replace "x:N","N" -replace '^<Win.*', '<Window'
 [XML]$XAML=$inputXAML
@@ -24,25 +24,43 @@ $xaml.SelectNodes("//*[@Name]") | ForEach-Object {
     }
 }
 
-#----------------------------------Plans--------------------------------------------#
 
-<#
-    Export only runs when file location is selected?
-    Perhaps have a running screen?
-#>
-
-
-#----------------------------------Search logic-------------------------------------#
-$var_searchButton.Add_Click({
-    $var_outputBox.Items.Clear()
-    Get-ADGroupFunction -groupSearch *$($var_inputBox.Text)*
-    
+#EXPORT BUTTON DEFAULT HANDLER
+$var_exportBox.IsEnabled = $false
+$var_outputBox.Add_SelectionChanged({
+    # Check if any item is selected
+    if ($var_outputBox.SelectedItem -ne $null) {
+        # Enable the export button if an item is selected
+        $var_exportBox.IsEnabled = $true
+    } else {
+        # Disable the export button if no item is selected
+        $var_exportBox.IsEnabled = $false
+    }
 })
-#----------------------------------DATA---------------------------------------------#
+
+#SCRIPT STATUS INDICATOR
+function UpdateStatusLabel{
+    param([Boolean]$status)
+
+    if ($status) {
+    $var_runningLabel.Visibility = "Visible"
+    }
+    else {
+    $var_runningLabel.Visibility = "Hidden"}
+}
+
+UpdateStatusLabel -status $false
+
+#DATA
 $data = [System.Collections.ArrayList]@()
 
-#----------------------------------EXPORT-------------------------------------------#
+#EXPORT FUNCTIONALITY
 $var_exportBox.Add_Click({
+UpdateStatusLabel -status $true
+$var_runningLabel.Visibility = "Visible"
+[System.Windows.Forms.MessageBox]::Show("Click 'Ok' to run the script...", "Script Running", "OK", "Information")
+write-host $var_runningLabel
+Start-Sleep -Milliseconds 100
 $uniqueEmails = @{}
 
 $selectedItem = $var_outputBox.SelectedItem
@@ -51,7 +69,7 @@ $data.Clear()
 
 Get-NestedGroupMembers -group $selectedItem
 
- # Prompt the user to select the destination path for the CSV file
+#DESTINATION PATH PROMPT
     $saveFileDialog = New-Object -TypeName System.Windows.Forms.SaveFileDialog
     $saveFileDialog.Filter = "CSV Files (*.csv)|*.csv"
     $saveFileDialog.Title = "Save CSV File"
@@ -64,37 +82,18 @@ $data | Export-Csv -Path $filePath -NoTypeInformation -Encoding UTF8
 elseif ($result -eq "Cancel") {
     [System.Windows.MessageBox]::Show("Export cancelled.", "Export Cancelled", "OK", "Warning")
 }
-##
+
+UpdateStatusLabel -status $false
+
 }
 
 )
 
-# ---UPDATE---
-function Get-ADGroupFunction {
-param ([string]$groupSearch) 
-#Get-ADGroup -Filter {Name -like $groupSearch}  -Properties * | select -property SamAccountName,Name
-$listOfGroups = Get-ADGroup -Filter {Name -like $groupSearch}  -Properties * | Select-Object -first 10 -expandproperty Name #limits it to the first 10 -> currently it doesn't search for the first x results
-foreach ($groupName in $listOfGroups) {
-$var_outputBox.Items.Add($groupName)
-write-host $groupName
-
-}
-}
-#Subgroup colours can be different from parent group, e.g. IT Services L1/L2 will have users of a random colour & they aren't the same
-
-#$checkbox.add_Indeterminate({ Write-Host 'indeterminate' })
-#Get-Service | ForEach-Object {$var_outputBox.Items.Add($_.Name)}
-
-$psform.ShowDialog()
-
-#-------------------------------------------SUB GROUP FUNCTION--------------------------------------#
+#SUB GROUP FUNCTION
 
 function Get-NestedGroupMembers {
-    param (
-        [string]$group,
-        [boolean]$londonFilter,
-        [boolean]$liverpoolFilter,
-        [boolean]$cambridgeFilter
+param (
+        [string]$group
     )
     $groupMembers = Get-ADGroupMember -Identity $group | Where-Object { $_.objectClass -eq 'user' }
 
@@ -103,25 +102,20 @@ function Get-NestedGroupMembers {
     foreach ($user in $groupMembers) {
         $userObject = Get-ADUser -Identity $user -Properties GivenName, Surname, UserPrincipalName, Office, info, Title, Department
         $userGroup = $group
-        write-host $userObject.Office
 
-
-##
         $upn = $userObject.UserPrincipalName
         if ([string]::IsNullOrEmpty($upn)) {
             $upn = "N/A"
         }
 
-        # Check if the email address is unique
+        #Check if the email address is unique
         if ($upn -ne "N/A" -and -not $uniqueEmails.ContainsKey($upn)) {
             $uniqueEmails[$upn] = $true
 
             $data.Add([PSCustomObject]@{
-                #Group = $userGroup
                 FirstName = $userObject.GivenName
                 LastName = $userObject.Surname
                 Email = $upn
-                #PAs = $userObject.info
                 Office = $userObject.Office
                 Title = $userObject.Title
                 Department = $userObject.Department
@@ -131,12 +125,26 @@ function Get-NestedGroupMembers {
         }
     }
 
-    # Recursively process nested groups
+    #Recursively process nested groups
     $nestedGroups = Get-ADGroupMember -Identity $group | Where-Object { $_.objectClass -eq 'group' }
 
     foreach ($nestedGroup in $nestedGroups) {
         Get-NestedGroupMembers -group $nestedGroup
     }
 }
-#----------------------------------------------------------------------------------------------------#
-# Make the UI better & a loading screen
+
+#SEARCH LOGIC
+$var_searchButton.Add_Click({
+    $var_outputBox.Items.Clear()
+    Get-ADGroupFunction -groupSearch *$($var_inputBox.Text)*
+    
+})
+#LIST BOX FUNCTIONALITY 
+function Get-ADGroupFunction {
+param ([string]$groupSearch) 
+$listOfGroups = Get-ADGroup -Filter {Name -like $groupSearch}  -Properties Name, SamAccountName | Select-Object -first 10
+foreach ($groupName in $listOfGroups) {
+$var_outputBox.Items.Add($groupName.SamAccountName)
+}
+}
+$psform.ShowDialog()
